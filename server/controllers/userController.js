99af4ac8225fe31955691ef992d6de17c1228a3b
Aniket Das <aniketdas1004@gmail.com>
2026-03-12 02:07:27 +0530

    addind cloudinary and update profile

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,7 @@
 import { generateToken } from "../lib/utils.js";
 import User from "../models/User.js";
 import bcrypt from "bcrypt.js"
+import cloudinary from "../lib/cloudinary.js";
 
 //SignUp a new user
 export const signup = async(req,res)=>{
@@ -37,7 +38,7 @@ export const login = async(req,res)=>{
     try {
         const{email,password}=req.body;
         const userData = await User.findOne({email})
-        const isPasswordCorrect = await bcrypt.compare(process,userData.password);
+        const isPasswordCorrect = await bcrypt.compare(password,userData.password);
 
         if(!isPasswordCorrect){
             return res.json({success: false,message:"Invalid credentials"});
@@ -55,4 +56,27 @@ export const login = async(req,res)=>{
 //Controller to check if user is authenticated
 export const checkAuth =(req,res)=>{
     res.json({success:true, user:req.user});
+}
+
+
+// Controller to update user profile details
+export const updateProfile = async (req,res)=>{
+    try {
+        const {profilePic,bio,fullName} =req.body;
+        const userId = req.user._id;
+        let updatedUser;
+
+        if(!profilePic){
+            updatedUser = await User.findByIdAndUpdate(userId,{bio,fullName},{new:true});
+        }else{
+            const upload = await cloudinary.uploader.upload(profilePic);
+
+            updatedUser = await User.findByIdAndUpdate(userId,{profilePic:upload.secure_url,bio,fullNmae},{new:true});
+        }
+
+        res.json({success:true,user:updatedUser})
+    } catch (error) {
+        console.log(error.message);
+        res.json({success:false,message:error.message})
+    }
 }
