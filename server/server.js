5ece7b761405333551f957773a531d5cca25e72c
Aniket Das <aniketdas1004@gmail.com>
2026-03-15 02:53:50 +0530

    socket io function defined

--- server/server.js
+++ server/server.js
@@ -17,6 +17,26 @@ export const io = new Server(server,{
     cors:{origin:"*"}
 })
 
+//Store online users
+export const userSocketMap ={};//{userId:socketId}
+
+//Socket.io connection handler
+io.on("connection",(socket)=>{
+    const userId=socket.handshake.query.userId;
+    console.log("User Connected",userId);
+
+    if(userId) userSocketMap[userId]=socket.id;
+
+    //Emit online users to all connected clients
+    io.emit("getOnlineUsers",Object.keys(userSocketMap));
+
+    socket.on("disconnect",()=>{
+        console.log("User disconnected",userId);
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers",Object.keys(userSocketMap))
+    })
+})
+
 //Middleware setup
 app.use(express.json({limit:"4mb"}));
 app.use(cors());
