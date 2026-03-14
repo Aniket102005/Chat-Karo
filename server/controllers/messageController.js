04fbb28320b7df8769c70ba7b5aa3c57a6dda22c
Aniket Das <aniketdas1004@gmail.com>
2026-03-14 13:09:20 +0530

    get all the messages for selected user and api to mark message

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -18,6 +18,39 @@ export const getUsersForSidebar = async(req,res)=>{
 
     } catch (error) {
         console.log(error.message);
-        res.json({success:false,message:filteredUsers,unseenMessages})
+        res.json({success:false,message:error.message})
     }
 }
+
+//Get all messages for selected user
+export const getMessages = async(req,res)=>{
+    try {
+        const {id:selectedUserId}=req.params;
+        const myId = req.user._id;
+
+        const messages=await Message.find({
+            $or:[
+                {senderId:myId,receiverId:selectedUserId},
+                {senderId:selectedUserId,receiverId:myId},
+            ]
+        })
+        await Message.updateMany({senderId:selectedUserId,receiverId:myId},{seen:true});
+        res.json({success:true,messages})
+        
+    } catch (error) {
+        console.log(error.message);
+        res.json({success:false,message:error.message})
+    }
+}
+
+//api to mark message as seen using message id
+export const markMessageAsSeen= async(req,res)=>{
+    try {
+        const{id}=req.params;
+        await Message.findByIdAndUpdate(id,{seen:true});
+        res.json({success:true});
+    } catch (error) {
+        console.log(error.message);
+        res.json({success:false,message:error.message});
+    }
+};
