655d473cdb2fdde9723452bfe70554d90da73896
Aniket Das <aniketdas1004@gmail.com>
2026-03-14 20:13:17 +0530

    initize socket io

--- server/server.js
+++ server/server.js
@@ -5,10 +5,17 @@ import http from "http";
 import { connect } from "http2";
 import { connectDB } from "./lib/db.js";
 import userRouter from "./routes/userRoutes.js";
+import messageRouter from "./routes/messageRoutes.js";
+import { Server } from "socket.io";
 
 // Create Express app and HTTP server
 const app = express();
 const server = http.createServer(app)
+
+//Intialize socket.io server
+export const io = new Server(server,{
+    cors:{origin:"*"}
+})
 
 //Middleware setup
 app.use(express.json({limit:"4mb"}));
@@ -17,7 +24,7 @@ app.use(cors());
 app.use("/api/status",(req,res)=>res.send("Server is live"));
 //Routes setup
 app.use("/api/auth",userRouter)
-
+app.use("api/messages",messageRouter);
 
 //Connect to MongoDB
 await connectDB();
