73d996573d96b99d696e60810c2f28a9d1dc373b
Aniket Das <aniketdas1004@gmail.com>
2026-03-15 13:08:11 +0530

    adding toaster and auth

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,12 @@ import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 import ProfilePage from './pages/ProfilePage'
 import assets from './assets/assets'
+import {Toaster} from "react-hot-toast"
 
 const App = () => {
   return (
     <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
+      <Toaster/>
       <Routes>
         <Route path='/' element={<HomePage/>}/>
         <Route path='/login' element={<LoginPage/>}/>
